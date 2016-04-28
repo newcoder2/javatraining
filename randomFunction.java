8436536fc09d3a4cd55abe7c6c5b4155329608f0
EinarCode <newcoder2@users.noreply.github.com>
2016-04-28 17:20:27 -0500

    Create randomFunction.java

---
+++ randomFunction.java
@@ -0,0 +1,12 @@
+private static int showRandom(int aStart, int aEnd, Random aRandom){
+		    if (aStart > aEnd) {
+		      throw new IllegalArgumentException("Start cannot exceed End.");
+		    }
+		    //get the range, casting to long to avoid overflow problems
+		    long range = (long)aEnd - (long)aStart + 1;
+		    // compute a fraction of the range, 0 <= frac < range
+		    long fraction = (long)(range * aRandom.nextDouble());
+		    int randomNumber =  (int)(fraction + aStart);    
+		    //log("Generated : " + randomNumber);
+			return randomNumber;
+		  }  
